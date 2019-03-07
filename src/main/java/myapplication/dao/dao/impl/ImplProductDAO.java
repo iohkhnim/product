03ac1f7fa0e9f41bbe85ce798a154f2cf6d351aa
iohnkhnim <hiepsi1211@gmail.com>
2019-03-08 02:15:24 +0700

    fix bugs

--- src/main/java/myapplication/dao/dao/impl/ImplProductDAO.java
+++ src/main/java/myapplication/dao/dao/impl/ImplProductDAO.java
@@ -1,10 +1,5 @@
 package myapplication.dao.dao.impl;
 
-import java.util.Calendar;
-import java.util.List;
-import java.util.TimeZone;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import myapplication.dao.IProductDAO;
 import myapplication.dto.Price;
 import myapplication.dto.Product;
@@ -16,20 +11,24 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.Calendar;
+import java.util.List;
+import java.util.TimeZone;
+
 @Transactional
 @Repository
 public class ImplProductDAO implements IProductDAO {
 
-  @PersistenceContext
-  private EntityManager entityManager;
+  @PersistenceContext private EntityManager entityManager;
 
   @SuppressWarnings("unchecked")
-  @Override
   public List<Product> findAll() {
     String hql = "FROM Product as prod ORDER BY prod.id";
     List<Product> list = (List<Product>) entityManager.createQuery(hql).getResultList();
     RestTemplate restTemplate = new RestTemplate();
-    //get product price
+    // get product price
     String url = "http://localhost:8085/price/findProductPrice/";
     ResponseEntity<String> response;
     for (Product item : list) {
@@ -39,45 +38,43 @@ public class ImplProductDAO implements IProductDAO {
     return list;
   }
 
-  @Override
   public Product findByid(int id) {
     Product prod = entityManager.find(Product.class, id);
-    //retrieve price from Price API
+    // retrieve price from Price API
     RestTemplate restTemplate = new RestTemplate();
     final String url = "http://localhost:8085/price/findProductPrice/";
     ResponseEntity<String> response = restTemplate.getForEntity(url + id, String.class);
     prod.setPrice(Integer.parseInt(response.getBody()));
 
-    //retrieve priceHistory
-    ResponseEntity<List<Price>> response2 = restTemplate.exchange(
-        "http://localhost:8085/price/findProductPriceHistory/"+id,
-        HttpMethod.GET,
-        null,
-        new ParameterizedTypeReference<List<Price>>(){});
+    // retrieve priceHistory
+    ResponseEntity<List<Price>> response2 =
+        restTemplate.exchange(
+            "http://localhost:8085/price/findProductPriceHistory/" + id,
+            HttpMethod.GET,
+            null,
+            new ParameterizedTypeReference<List<Price>>() {});
     List<Price> list = response2.getBody();
     prod.setPriceHistory(list);
 
     return prod;
   }
 
-  @Override
   public Boolean create(Product product) {
     try {
       product.setCreatedTime(Calendar.getInstance(TimeZone.getTimeZone("GMT")).getTime());
       product.setUpdatedTime(Calendar.getInstance(TimeZone.getTimeZone("GMT")).getTime());
 
-      //create new Product
+      // create new Product
       entityManager.persist(product);
 
-      //create new Price
+      // create new Price
       final String url = "http://localhost:8085/price/create";
       Price price = new Price();
       price.setPrice(product.getPrice());
       price.setProduct_id(product.getId());
       RestTemplate restTemplate = new RestTemplate();
       HttpEntity<Price> requestBody = new HttpEntity<>(price);
-      ResponseEntity<Price> result
-          = restTemplate.postForEntity(url, requestBody, Price.class);
+      ResponseEntity<Price> result = restTemplate.postForEntity(url, requestBody, Price.class);
 
       return true;
     } catch (Exception ex) {
@@ -85,10 +82,9 @@ public class ImplProductDAO implements IProductDAO {
     }
   }
 
-  @Override
   public Boolean update(Product product) {
     try {
-      int flag = 0; //mark if price is changed
+      int flag = 0; // mark if price is changed
       Product prod = findByid(product.getId());
       prod.setName(product.getName());
       prod.setSupplier(product.getSupplier());
@@ -98,7 +94,7 @@ public class ImplProductDAO implements IProductDAO {
       }
       prod.setUpdatedTime(Calendar.getInstance(TimeZone.getTimeZone("GMT")).getTime());
       entityManager.flush();
-      //create new Price
+      // create new Price
       if (flag == 1) {
         final String url = "http://localhost:8085/price/create";
         Price price = new Price();
@@ -106,8 +102,7 @@ public class ImplProductDAO implements IProductDAO {
         price.setPrice(prod.getPrice());
         RestTemplate restTemplate = new RestTemplate();
         HttpEntity<Price> requestBody = new HttpEntity<>(price);
-        ResponseEntity<Price> result
-            = restTemplate.postForEntity(url, requestBody, Price.class);
+        ResponseEntity<Price> result = restTemplate.postForEntity(url, requestBody, Price.class);
       }
       return true;
     } catch (Exception ex) {
@@ -115,12 +110,11 @@ public class ImplProductDAO implements IProductDAO {
     }
   }
 
-  @Override
   public Boolean delete(int id) {
     try {
       entityManager.remove(findByid(id));
-      //delete all Price related to this id
-      final String url = "http://localhost:8085/price/deleteByProductId/"+id;
+      // delete all Price related to this id
+      final String url = "http://localhost:8085/price/deleteByProductId/" + id;
       RestTemplate restTemplate = new RestTemplate();
       restTemplate.delete(url);
       return true;
