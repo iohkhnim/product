d38fee930072d493471fa4b13547db6fb0fdc612
Khoi Nguyen Minh <intern.nmkhoi@tma.com.vn>
2019-03-13 11:16:48 +0700

    add supplier grpc client

---
+++ src/main/java/myapplication/service/service/impl/ProductServiceGrpcImpl.java
@@ -0,0 +1,28 @@
+package myapplication.service.service.impl;
+
+import com.khoi.productproto.GetProductRequest;
+import com.khoi.productproto.ProductEntry;
+import com.khoi.productproto.ProductServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import myapplication.dao.IProductDAO;
+import myapplication.dto.Product;
+import org.lognet.springboot.grpc.GRpcService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@GRpcService
+public class ProductServiceGrpcImpl extends ProductServiceGrpc.ProductServiceImplBase {
+
+  @Autowired
+  private IProductDAO productDAO;
+
+  @Override
+  public void getProduct(GetProductRequest request, StreamObserver<ProductEntry> resonseObserver) {
+    Product product = productDAO.findByid(request.getProductId());
+    resonseObserver.onNext(
+        ProductEntry.newBuilder().setId(product.getId()).setName(product.getName())
+            .setDescription(product.getDescription())
+            .setCreatedTime(product.getCreatedTime().getTime())
+            .setUpdatedTime(product.getUpdatedTime().getTime()).build());
+    resonseObserver.onCompleted();
+  }
+}
