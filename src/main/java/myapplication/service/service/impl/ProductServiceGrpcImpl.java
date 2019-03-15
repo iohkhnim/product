f0545eca80d4372dbb6e1993a9f53fd21b34bc4d
Khoi Nguyen Minh <intern.nmkhoi@tma.com.vn>
2019-03-15 10:30:41 +0700

    change datetime from int to string

--- src/main/java/myapplication/service/service/impl/ProductServiceGrpcImpl.java
+++ src/main/java/myapplication/service/service/impl/ProductServiceGrpcImpl.java
@@ -4,6 +4,9 @@ import com.khoi.productproto.GetProductRequest;
 import com.khoi.productproto.ProductEntry;
 import com.khoi.productproto.ProductServiceGrpc;
 import io.grpc.stub.StreamObserver;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import myapplication.dao.IProductDAO;
 import myapplication.dto.Product;
 import org.lognet.springboot.grpc.GRpcService;
@@ -15,14 +18,19 @@ public class ProductServiceGrpcImpl extends ProductServiceGrpc.ProductServiceImp
   @Autowired
   private IProductDAO productDAO;
 
+  private String convertDate2String(Date date){
+    DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+    return dateFormat.format(date);
+  }
+
   @Override
   public void getProduct(GetProductRequest request, StreamObserver<ProductEntry> responseObserver) {
     Product product = productDAO.findByid(request.getProductId());
     responseObserver.onNext(
         ProductEntry.newBuilder().setId(product.getId()).setName(product.getName())
             .setDescription(product.getDescription())
-            .setCreatedTime(product.getCreatedTime().getTime())
-            .setUpdatedTime(product.getUpdatedTime().getTime()).build());
+            .setCreatedTime(convertDate2String(product.getCreatedTime()))
+            .setUpdatedTime(convertDate2String(product.getUpdatedTime())).build());
     responseObserver.onCompleted();
   }
 }
