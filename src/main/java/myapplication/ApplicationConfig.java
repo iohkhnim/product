68d049be9cbecb6d08143ef43e07d3cde657bd1f
Khoi Nguyen Minh <intern.nmkhoi@tma.com.vn>
2019-03-25 17:28:41 +0700

    add authentication

--- src/main/java/myapplication/ApplicationConfig.java
+++ src/main/java/myapplication/ApplicationConfig.java
@@ -5,6 +5,10 @@ import com.khoi.stockproto.StockServiceGrpc;
 import com.khoi.supplierproto.SupplierServiceGrpc;
 import io.grpc.Channel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.netty.GrpcSslContexts;
+import io.grpc.netty.NegotiationType;
+import io.grpc.netty.NettyChannelBuilder;
+import java.io.File;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,8 +21,11 @@ public class ApplicationConfig {
   private final String supplierServiceEndpoint = "localhost:6580";
 
   @Bean(name = "priceChannel")
-  Channel priceChannel() {
-    return ManagedChannelBuilder.forTarget(priceServiceEndpoint).usePlaintext().build();
+  Channel priceChannel() throws Exception {
+    return NettyChannelBuilder.forTarget(priceServiceEndpoint).negotiationType(NegotiationType.TLS)
+        .sslContext(GrpcSslContexts.forClient().trustManager(new File("D:\\product\\src\\main\\java\\myapplication\\auth\\ca.crt")).build())
+        .build();
+    //return ManagedChannelBuilder.forTarget(priceServiceEndpoint).usePlaintext().build();
   }
 
   @Bean(name = "stockChannel")
